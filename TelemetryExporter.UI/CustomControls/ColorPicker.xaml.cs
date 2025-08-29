6603570c74f8ba1da93f72ff637ab9d29ed6e57f
Mihail Yankov <mihail.yankov.1337@gmail.com>
2025-08-30 00:02:38 +0300

    #63 Color picker almost done. Adjust when color is provided from outside.

--- TelemetryExporter.UI/CustomControls/ColorPicker.xaml.cs
+++ TelemetryExporter.UI/CustomControls/ColorPicker.xaml.cs
@@ -4,59 +4,68 @@ using CommunityToolkit.Maui.Core.Extensions;
 
 namespace TelemetryExporter.UI.CustomControls;
 
-public partial class ColorPicker : ContentView, INotifyPropertyChanged
+public partial class ColorPicker : ContentView
 {
     private double maxPickerX = 0;
     private double accumulatedPickerSaturation = 0;
     private double accumulatedHSVValue = 0;
 
-    private const double hsvLayersHeight = 130;
-    private const int marginBuffer = 2;
+    private bool adjustColorPickerPosition;
+
+    private const double HsvLayersHeight = 130;
+    private const int MarginBuffer = 2;
+
+    private Color? selectedColor;//= Colors.Red;
 
     /// <summary>
     /// null means no color selected, reset.
     /// </summary>
     public event EventHandler<Color?>? OnColorChanged;
 
-    private Color? selectedColor;//= Colors.Red;
-    public Color? SelectedColor 
-    { 
-        get => selectedColor; 
+    public readonly BindableProperty SelectedColorProperty =
+        BindableProperty.Create(
+            nameof(SelectedColor),
+            typeof(Color),
+            typeof(ColorPicker),
+            null, // Colors.Red
+            BindingMode.TwoWay);
+
+
+    //public Color? SelectedColor 
+    //{ 
+    //    get => selectedColor; 
+    //    set
+    //    {
+    //        selectedColor = value;
+    //        OnColorChanged?.Invoke(this, selectedColor);
+    //        OnPropertyChanged(nameof(SelectedColor));
+    //    }
+    //}
+
+    public Color? SelectedColor
+    {
+        get => (Color?)GetValue(SelectedColorProperty);
         set
         {
+            if (selectedColor == null && value != null)
+            {
+                // InitializeColorPickerXYPostion(value);
+                adjustColorPickerPosition = true;
+            }
+
+            SetValue(SelectedColorProperty, value);
             selectedColor = value;
             OnColorChanged?.Invoke(this, selectedColor);
-            OnPropertyChanged(nameof(SelectedColor));
         }
     }
 
     public ColorPicker()
     {
-        
+        // Test with each side of the colorpicker at the edge, or at corners.
+        // Where to set the default color
         InitializeComponent();
 
-        // NOt working when color set from XAML,from the user component
-
-        // if (selectedColor is null)
-        // set values slider according to the hue value of the selected color
-        // set colorPickerEllipse position according to the saturation and value of the selected color
-        // transationX and transtionY and belare of the marginBuffer
-        if (SelectedColor is not null)
-        {
-            hueSlider.Value = SelectedColor.GetHue();
-            accumulatedPickerSaturation = -(SelectedColor.GetSaturation() * (Width - marginBuffer));
-            accumulatedHSVValue = SelectedColor.GetPercentBlackKey() * (hsvLayersHeight - marginBuffer);
-            colorPickerEllipse.TranslationX = accumulatedPickerSaturation;
-            colorPickerEllipse.TranslationY = accumulatedHSVValue;
-            hueLayerStop.Color = Color.FromHsv((int)hueSlider.Value, 100, 100);
-            hueSlider.ThumbColor = hueLayerStop.Color;
-        }else
-        {
-            selectedColor = Colors.Red;
-        }
-
-
-            this.secondRowDefinition.Height = new GridLength(hsvLayersHeight);
+        this.secondRowDefinition.Height = new GridLength(HsvLayersHeight);
 
         PanGestureRecognizer dragRegognizer = new();
         dragRegognizer.PanUpdated += DragRegognizer_PanUpdated; ;
@@ -80,6 +89,11 @@ public partial class ColorPicker : ContentView, INotifyPropertyChanged
         if (width > 0)
         {
             maxPickerX = width;
+
+            if (this.adjustColorPickerPosition)
+            {
+                InitializeColorPickerXYPostion(SelectedColor);
+            }
         }
     }
 
@@ -90,7 +104,11 @@ public partial class ColorPicker : ContentView, INotifyPropertyChanged
             Color newHue = Color.FromHsv((int)e.NewValue, 100, 100);
             slider.ThumbColor = hueLayerStop.Color = newHue;
 
-            this.SelectedColor = this.SelectedColor.WithHue(newHue.GetHue());
+            // this event is also triggered when setting the slider value from code when "SelectedColor" is still null
+            if (this.SelectedColor != null)
+            {
+                this.SelectedColor = this.SelectedColor.WithHue(newHue.GetHue());
+            }
         }
     }
 
@@ -99,25 +117,38 @@ public partial class ColorPicker : ContentView, INotifyPropertyChanged
         switch (e.StatusType)
         {
             case GestureStatus.Running:
-                colorPickerEllipse.TranslationX = Math.Clamp(accumulatedPickerSaturation + e.TotalX, -Width + marginBuffer, 0);
-                colorPickerEllipse.TranslationY = Math.Clamp(accumulatedHSVValue + e.TotalY, 0, hsvLayersHeight - marginBuffer);
+                colorPickerEllipse.TranslationX = Math.Clamp(accumulatedPickerSaturation + e.TotalX, -Width + MarginBuffer, 0);
+                colorPickerEllipse.TranslationY = Math.Clamp(accumulatedHSVValue + e.TotalY, 0, HsvLayersHeight - MarginBuffer);
                 break;
             case GestureStatus.Completed:
                 accumulatedPickerSaturation = colorPickerEllipse.TranslationX;
                 accumulatedHSVValue = colorPickerEllipse.TranslationY;
 
-                /// Color c = Color.FromHsv((int)this.hueSlider.Value, 100, 100);
+                // Color c = Color.FromHsv((int)this.hueSlider.Value, 100, 100);
                 var h = hueLayerStop.Color.GetHue();
 
-                float s = (float)(accumulatedPickerSaturation / (-Width + marginBuffer));
-                float v = (float)(accumulatedHSVValue / (hsvLayersHeight - marginBuffer));
+                float s = (float)(accumulatedPickerSaturation / (-Width + MarginBuffer));
+                float v = (float)(accumulatedHSVValue / (HsvLayersHeight - MarginBuffer));
 
-                this.SelectedColor = Color.FromHsv(h, 1- s, 1- v);
+                this.SelectedColor = Color.FromHsv(h, 1 - s, 1 - v);
                 ;
 
                 break;
             default:
                 break;
         }
+    }
+
+    private void InitializeColorPickerXYPostion(Color newValue)
+    {
+        // hue is between 0 and 1, slider is between 0 and 360
+        // this also triggers the Slider_ValueChanged event, which sets the hueLayerStop.Color and the slider.ThumbColor
+        hueSlider.Value = 360 * newValue.GetHue();
+
+        double saturationX = -(Width - (newValue.GetSaturation() * Width));
+        double hsvValueY = newValue.GetPercentBlackKey() * (HsvLayersHeight - MarginBuffer);
+
+        DragRegognizer_PanUpdated(null, new PanUpdatedEventArgs(GestureStatus.Running, 1, saturationX, hsvValueY));
+        DragRegognizer_PanUpdated(null, new PanUpdatedEventArgs(GestureStatus.Completed, 1));
     }
 }
